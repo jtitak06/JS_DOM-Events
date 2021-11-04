e424f7a5157dcdd9e02c76d84f7587102318c19a
Jeff Titak <jtitak06@yahoo.com>
2021-11-04 16:02:12 +0300

    commit

--- main.js
+++ main.js
@@ -9,6 +9,10 @@ console.log(document.getElementsByClassName("node2"));
 document.getElementsByClassName("node2")[0].textContent =
   'I used the getElementByClassName("node2") method to access this';
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+//const h3 = document.querySelectorAll("h3");
+//for (let ele of h3) {
+//  ele.textContent = 'I used the getElementByTagName("h3") method to access all of these';
+//}
 for (const ele of document.getElementsByTagName("h3")) {
   ele.textContent =
     'I used the getElementByTagName("h3") method to access all of these';
@@ -20,20 +24,20 @@ for (const ele of document.getElementsByTagName("h3")) {
 let newP1 = document.createElement("p");
 newP1.textContent = "This node was created using the createElement() method";
 // TODO: Append the created node to the parent node using the element.appendChild() method
+
 let parent1 = document.querySelector("#parent");
 parent1.appendChild(newP1);
 // TODO: Create a <a> element using this element.createElement() and put this text inside "I am a <a> tag"
 let anchor1 = document.createElement("a");
 anchor1.textContent = "Google Search";
-// BONUS: Add a link href to the <a>
+//BONUS: Add a link href to the <a>
 anchor1.href = "https://google.com";
 anchor1.target = "_blank";
 let externalLinkIcon = document.createElement("img");
 externalLinkIcon.src = "./assets/external-link.svg";
 externalLinkIcon.style.width = "25px";
 externalLinkIcon.style.color = "blue";
-// TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
-parent1.insertBefore(anchor1, newP1);
+//TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method/parent1.insertBefore(anchor1, newP1);
 anchor1.appendChild(externalLinkIcon);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
@@ -58,7 +62,7 @@ let dir = "right";
 let box = document.querySelector("#box");
 let container = document.querySelector("#container");
 // TODO: Write your JavaScript here to make the red box go from left to right
-// let interval = setInterval(move, 10);
+let interval = setInterval(move, 10);
 
 function move() {
   console.log(posX, posY);
@@ -101,10 +105,10 @@ function move() {
   }
 }
 
-// BONUS - Make the red box go all the way around the perimeter of the green box
-
-/*----------- Exercise #5: DOM EVENTS --------------*/
-
+//// BONUS - Make the red box go all the way around the perimeter of the green box
+//
+///*----------- Exercise #5: DOM EVENTS --------------*/
+//
 // TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
 // -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
 // This div should be a 'modal' that covers the main content on the screen
